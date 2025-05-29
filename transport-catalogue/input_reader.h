1ac00b99db24f098ffaca403658cbaca3702b655
MakuMutts <kazminaviktoriya@gmail.com>
2025-05-29 15:08:50 +0300

    input_reader.h v1.0

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,38 @@
-// место для вашего кода
+#pragma once
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include "geo.h"
+#include "transport_catalogue.h"
+
+namespace input_reader {
+
+namespace detail {
+
+struct CommandDescription {
+    explicit operator bool() const {
+        return !command.empty();
+    }
+
+    bool operator!() const {
+        return !operator bool();
+    }
+
+    std::string command;
+    std::string id;
+    std::string description;
+};
+
+} //detail
+
+class InputReader {
+public:
+    void ParseLine(std::string_view line);
+    void ApplyCommands(transport_catalogue::TransportCatalogue& catalogue) const;
+
+private:
+    std::vector<detail::CommandDescription> commands_;
+};
+
+} //input_reader
