c7abf33349613dc481cba79824b4b313dfb061df
MakuMutts <kazminaviktoriya@gmail.com>
2025-05-29 15:09:53 +0300

    stat_reader.h v1.0

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,13 @@
-// место для вашего кода
+#pragma once
+
+#include <iosfwd>
+#include <string_view>
+
+#include "transport_catalogue.h"
+
+namespace stat_reader {
+
+void ParseAndPrintStat(const transport_catalogue::TransportCatalogue& tansport_catalogue, 
+                      std::string_view request, std::ostream& output);
+
+} 
